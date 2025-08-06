bef6997276d9bcabb825bca72a715c04129c4fb5
Puja <pujakeshri098765@gmail.com>
2025-08-06 08:57:47 +0530

    Routing

---
+++ src/Body.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+const Body = () => {
+  return (
+    <div>
+      <NavBar/>
+       {/* child routes render over */}
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Body
